5f63580958724f355155d0725ae0a3e5f8c7c873
Mohamed <mohamed@gmail.com>
2026-08-26 19:25:18 +0300

    Add new task

--- script.js/session13.js
+++ script.js/session13.js
@@ -1,14 +1,36 @@
-function looping(){
-    let start = Number(prompt("enter start:"));
-    let end = Number(prompt("enter end:"));
-    for (let i =start; i<=end;i++){
-        if(i===5){
-            continue;
-        }
-        if(i===8){
+// function looping(){
+//     let start = Number(prompt("enter start:"));
+//     let end = Number(prompt("enter end:"));
+//     for (let i =start; i<=end;i++){
+//         if(i===5){
+//             continue;
+//         }
+//         if(i===8){
+//             break;
+//         }
+//  console.log(i);
+//     }
+//     }
+// looping();
+let tracks= ["web design" , "network" , "ux"]
+function looping() {
+    let tracks = ["Web Design", "Network", "UX"];
+
+    let search = prompt("Enter track:");
+
+    let found = false;
+
+    for (let i = 0; i < tracks.length; i++) {
+        if (tracks[i] == search) {
+            console.log(tracks[i]);
+            found = true;
             break;
         }
- console.log(i);
     }
+
+    if (found == false) {
+        console.log(tracks);
     }
+}
+
 looping();
